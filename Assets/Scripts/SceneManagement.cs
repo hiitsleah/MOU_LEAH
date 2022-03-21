c87539b36e560c55cbeae74e9dd56dd5f2ef983f
Junru Angelina Xu <anhsuxjr@gmail.com>
2022-03-21 14:02:31 -0400

    startButton
    create StartButton & scene management

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    string sceneName;
+
     void Start()
     {
-        
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        sceneName = currentScene.name;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    //for menu interface use
+
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("Main");
     }
+
+    public void QuitGame()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene("Menu");
+    }
+
+    //for in game coding
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (sceneName == "Menu")
+            {
+                SceneManager.LoadScene("Menu");
+            }
+
+            //if (sceneName == "Level_1" && GlobalVariables.hasKey == true)
+            //{
+            //    SceneManager.LoadScene("Level_2");
+            //}
+
+            //if (sceneName == "Level_2")
+            //{
+            //    SceneManager.LoadScene("Level_3");
+            //}
+
+        }
+
+    }
+
 }
