a2bdef9d88edbd8a2f284ecc0d3267063a3866bc
Junru Angelina Xu <anhsuxjr@gmail.com>
2022-03-21 17:10:36 -0400

    adding Fogs overlay & interaction

--- Assets/Scripts/InteractionController.cs
+++ Assets/Scripts/InteractionController.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//using UnityEngine.UI;
 public class InteractionController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject sign1;
+    public GameObject Rain;
+    public AudioClip panelOpen;
+    //public GameObject piclight;
+    //public AudioClip panelClose;
+    AudioSource audioSrc;
+
     void Start()
     {
+        sign1.SetActive(false);
         
+        Rain.SetActive(true);
+        //piclight.SetActive(false);
+        audioSrc = GetComponent<AudioSource>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player Entered");
+            sign1.SetActive(true);
+            Rain.SetActive(false);
+            //piclight.SetActive(true);
+            //audioSrc(panelOpen);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerExit(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            Rain.SetActive(true);
+            //piclight.SetActive(false);
+            Debug.Log("Player Exited");
+            ////        sign1.SetActive(false);
+            ////        pic1.SetActive(false);
+            ////        audioSrc.PlayOneShot(panelClose);
+        }
     }
+
 }
