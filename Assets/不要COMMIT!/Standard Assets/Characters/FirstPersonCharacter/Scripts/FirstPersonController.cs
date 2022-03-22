96a08e0333dd3d1812b48c26447bdc5077454a6f
Junru Angelina Xu <anhsuxjr@gmail.com>
2022-03-22 13:58:37 -0400

    debug

--- Assets/不要COMMIT!/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/不要COMMIT!/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -4,6 +4,10 @@ using UnityStandardAssets.CrossPlatformInput;
 using UnityStandardAssets.Utility;
 using Random = UnityEngine.Random;
 
+#if UNITY_WEBGL && !UNITY_EDITOR
+        lookSpeed *= .65f;
+#endif
+
 #pragma warning disable 618, 649
 namespace UnityStandardAssets.Characters.FirstPerson
 {
