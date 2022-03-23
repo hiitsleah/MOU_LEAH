39235d37b30d176485e3f0a8c8f479e2d931e3e8
Junru Angelina Xu <anhsuxjr@gmail.com>
2022-03-22 22:00:05 -0400

    FINAL debug GOOD TO GOOO

---
+++ Assets/Scripts/VideoPlayer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayer : MonoBehaviour
+{
+    public string url;
+    VideoPlayer vidPlayer;  
+
+    void Start()
+    {
+        vidPlayer = GetComponent<VideoPlayer>();
+        vidPlayer.url = url;
+    }
+
+  
+    void Update()
+    {
+        Play();
+
+    }
+
+
+    void Play()
+    {
+        vidPlayer.Play();
+        //vidPlayer.isLooping = true;
+
+
+    }
+}
